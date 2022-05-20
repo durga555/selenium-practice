2fd6a97b4afbe42e3e9ac4c7eee565cef9484cb3
durga555 <prasadsdp555@gmail.com>
2022-05-20 18:53:46 +0530

    second comment

--- src/test/java/properties/test/ReadConfig1.java
+++ src/test/java/properties/test/ReadConfig1.java
@@ -5,6 +5,8 @@ import java.io.FileNotFoundException;
 import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -12,7 +14,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class ReadConfig1 {
 	static WebDriver driver;
 	
-   static
+   
 	public static void main(String[] args) throws Exception {
 		WebDriverManager.firefoxdriver().setup();
 		driver = new FirefoxDriver(); 
@@ -21,12 +23,29 @@ public class ReadConfig1 {
 		FileInputStream fi = new FileInputStream("C:\\Users\\durga prasad\\eclipse-workspace\\seleniumi1\\configuration\\configuration1\\read1.properties");
 		Properties p = new Properties();
 		p.load(fi); 
+		String name = p.getProperty("browser");
+	
+		switch(name) {
+		case "chrome":
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+			break;
+		case "firefox":
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+			break;
+		case "edge":
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+			break;
+			
+		}
 		System.out.println (p.getProperty("browser"));
 		System.out.println(p.getProperty("url"));
-		switch () {
 		
-		}
-	}
+		
+   }
+}
 	
 
-}
+
